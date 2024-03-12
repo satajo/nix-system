11fa59df69fe03a692f936ff6aa11e53b90f64c2
Sami Jokela <sami.jokela@satajo.com>
2024-03-12 16:59:17 +0100

    Generate hardware configuration on the fly
    Use a relatively hacky solution to work around the impure usage, but because
    the system configuration is an entrypoint to the configuration, it should be
    ok to do this. Nobody needs to import this, right

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
 #!/bin/sh -e
 
+# Ensure hardware-cofiguration.nix exists.
+if [ ! -f hardware-configuration.nix ]; then
+  echo "NixOS hardware configuration file not found - Regenerating..."
+  sudo nixos-generate-config --show-hardware-config > hardware-configuration.nix
+
+  # Git hack to expose the generated hardware-configuration.nix to Git without
+  # actually committing it in git. In lieu of granular impurity control mechanisms,
+  # this is the best we can do.
+  #
+  # For details, see: https://discourse.nixos.org/t/can-i-use-flakes-within-a-git-repo-without-committing-flake-nix/18196
+  git add --force --intent-to-add hardware-configuration.nix
+  git update-index --assume-unchanged hardware-configuration.nix
+fi
+
 sudo nixos-rebuild switch --flake .#default
