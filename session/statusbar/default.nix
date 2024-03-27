c0275d702edc0b323816d8a949376accd02d05b8
Sami Jokela <sami.jokela@satajo.com>
2024-03-27 12:16:04 +0200

    Add media status display blocks to Polybar
    Both current system volume and playing media is displayed.

--- session/statusbar/default.nix
+++ session/statusbar/default.nix
@@ -1,10 +1,8 @@
 { pkgs, ... }:
 let theme = import ../../theme/lib.nix { pkgs = pkgs; };
 in {
-  #environment.systemPackages = with pkgs; [ polybarFull ];
-
   home-manager.users.satajo = {
-    home.packages = with pkgs; [ polybarFull ];
+    home.packages = with pkgs; [ playerctl polybarFull ];
 
     xdg.configFile."polybar/config.ini".source =
       theme.substitute ./config.template.ini;
