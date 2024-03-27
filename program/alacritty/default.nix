fd6366fd583f842a3bcc3190cee78b44609902f4
Sami Jokela <sami.jokela@satajo.com>
2024-03-27 16:59:42 +0200

    Take some alacritty colors from the theme colors
    The theme really should be expanded to cover more options; having the colors be
    hardcoded makes no sense. Maybe look into the base16 or base24 color schemes?

--- program/alacritty/default.nix
+++ program/alacritty/default.nix
@@ -1,10 +1,9 @@
-{ config, pkgs, ... }: {
-
+{ config, pkgs, ... }:
+let theme = import ../../theme/lib.nix { pkgs = pkgs; };
+in {
   home-manager.users.satajo = {
-
     home.packages = with pkgs; [ alacritty ];
-
-    xdg.configFile."alacritty.yml".source = ./alacritty.yml;
-
+    xdg.configFile."alacritty.yml".source =
+      theme.substitute ./alacritty.template.yml;
   };
 }
