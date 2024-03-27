8be9d27a2d31b9f5b1e0cfe97537ec390b6c8328
Sami Jokela <sami.jokela@satajo.com>
2024-03-27 10:48:00 +0200

    Add more programs

--- program/default.nix
+++ program/default.nix
@@ -18,12 +18,16 @@
   # Userspace single-package programs with no configuration.
   home-manager.users.satajo.home.packages = with pkgs;
     [
+      evince # document viewer
+      feh # image viewer
       firefox # browser
       hyperfine # benchmarking tool
       neofetch # it's basically required, right?
       nil # nix language server
+      spotify
       tldr # console command cheatsheets
       vscode # code editor / ide
+      vlc # media player
     ] ++ lib.optionals config.profile.personal [
       bitwarden # password manager
       discord # chat
