bf8bfa624073845ec7d09c5e0391c3961f2f677a
Sami Jokela <sami.jokela@satajo.com>
2024-04-14 23:43:50 +0300

    Add more programs

--- program/default.nix
+++ program/default.nix
@@ -4,14 +4,17 @@
 
   # System wide single-package programs with no configuration.
   environment.systemPackages = with pkgs; [
+    bc # calculator
     coreutils # GNU coreutils
     curl
     htop
     git
     nano
     pkg-config
+    ripgrep
     tig # Git terminal gui
     wget
+    units # conversion between different units
     unzip # .zip extraction
   ];
 
@@ -28,6 +31,12 @@
       tldr # console command cheatsheets
       vscode # code editor / ide
       vlc # media player
+
+      ## Kubernetes and virtualization
+      minikube # local k8s deployment
+      kubectl
+      k9s # kubectl terminal ui
+      skaffold # kubernetes deployer / dev environment
     ] ++ lib.optionals config.profile.personal [
       bitwarden # password manager
       discord # chat
