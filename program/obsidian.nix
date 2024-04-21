1f6dc1375d2fef183b408fe75156fff7932d9e28
Sami Jokela <sami.jokela@satajo.com>
2024-04-21 13:35:04 +0300

    Autostart Obsidian on session startup

--- program/obsidian.nix
+++ program/obsidian.nix
@@ -4,5 +4,19 @@
 
     # Obsidian depends on electron which is outdated.
     nixpkgs.config.permittedInsecurePackages = [ "electron-25.9.0" ];
+
+    systemd.user.services.obsidian-desktop = {
+      description = "Obsidian desktop client";
+      after = [ "graphical-session.target" ];
+      wantedBy = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
+
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.obsidian}/bin/obsidian";
+        Restart = "on-failure";
+        RestartSec = "1s";
+      };
+    };
   };
 }
