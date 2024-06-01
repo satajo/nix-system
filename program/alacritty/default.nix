1c2089c75da3d5d75913edfbcd5141c6fc057808
Sami Jokela <sami.jokela@satajo.com>
2024-06-01 21:08:37 +0300

    Migrate alacritty.yml to toml

--- program/alacritty/default.nix
+++ program/alacritty/default.nix
@@ -3,7 +3,7 @@ let theme = import ../../theme/lib.nix { pkgs = pkgs; };
 in {
   home-manager.users.satajo = {
     home.packages = with pkgs; [ alacritty ];
-    xdg.configFile."alacritty.yml".source =
-      theme.substitute ./alacritty.template.yml;
+    xdg.configFile."alacritty.toml".source =
+      theme.substitute ./alacritty.template.toml;
   };
 }
