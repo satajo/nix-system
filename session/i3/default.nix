71d87659540343cd6d039adbcc3285fd7ac1de9e
Sami Jokela <sami.jokela@satajo.com>
2024-10-19 13:30:06 +0200

    Add wallpaper image with theming support

--- session/i3/default.nix
+++ session/i3/default.nix
@@ -1,6 +1,15 @@
 { pkgs, ... }:
 let
   theme = import ../../theme/lib.nix { pkgs = pkgs; };
+
+  applyWallpaperTheme =
+    image:
+    pkgs.runCommand "wallpaper.png" { nativeBuildInputs = [ pkgs.imagemagick ]; } ''
+      magick ${image} -colorspace RGB \
+        -fill "${theme.color.layer2.background}" -opaque "#ffffff" \
+        -fill "${theme.color.layer1.background}" -opaque "#000000" \
+        $out
+    '';
 in
 {
   environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
@@ -15,11 +24,12 @@ in
 
     windowManager.i3 = {
       enable = true;
-      extraPackages = with pkgs; [ hsetroot ];
+      extraPackages = with pkgs; [ feh ];
     };
   };
 
   home-manager.users.satajo = {
     xdg.configFile."i3/config".source = theme.substitute ./config.template;
+    xdg.configFile."i3/wallpaper.png".source = applyWallpaperTheme ./wallpaper.png;
   };
 }
