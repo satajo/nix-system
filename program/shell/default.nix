47d4d16f7a2e4ec30bda83a46b62029e89b9ad68
Sami Jokela <sami.jokela@satajo.com>
2024-12-08 12:29:52 +0100

    Configure shell more idiomatically

--- program/shell/default.nix
+++ program/shell/default.nix
@@ -1,18 +1,36 @@
 { config, pkgs, ... }:
 
 {
-  home-manager.users.satajo = {
+  # Configure bash
+  programs.bash = {
+    interactiveShellInit = ''
+      # Increase history size from the defaults.
+      HISTSIZE=10000
+      HISTFILESIZE=20000
 
-    home.packages = with pkgs; [
-      bash
-      fzf
-      starship
-    ];
+      # Ignore commands with leading space, and keep only one duplicate around.
+      HISTCONTROL=ignorespace:ignoredups:erasedups
 
-    # Bash
-    home.file.".bashrc".source = ./bashrc;
+      # Append to the history file, don't overwrite it.
+      shopt -s histappend
 
-    # Starship shell
-    xdg.configFile."starship.toml".source = ./starship.toml;
+      # Append to history immediately after each command
+      PROMPT_COMMAND="history -a"
+
+      # Set window title to the working directory.
+      PROMPT_COMMAND="$PROMPT_COMMAND; echo -ne \"\033]0;Terminal - \$PWD\007\""
+    '';
+  };
+
+  # fzf
+  programs.fzf = {
+    keybindings = true;
+    fuzzyCompletion = true;
+  };
+
+  # Starship command prompt
+  programs.starship = {
+    enable = true;
+    settings = pkgs.lib.importTOML ./starship.toml;
   };
 }
