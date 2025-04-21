2f401e8eb075402fdcb99dd8506566e12a779992
Sami Jokela <sami.jokela@satajo.com>
2025-04-21 19:54:00 +0200

    Add gamemoderun to steam configuration

--- program/steam.nix
+++ program/steam.nix
@@ -6,9 +6,15 @@
 }:
 {
   config = lib.mkIf config.profile.personal {
+    # Steam launch option: gamemoderun mangohud %command%
     programs.steam = {
       enable = true;
       extraPackages = with pkgs; [ mangohud ];
     };
+
+    # Enable gamemode to optimize in-game performance.
+    # Check configuration using `gamemoded -t`.
+    programs.gamemode.enable = true;
+    users.extraGroups.gamemode.members = [ "satajo" ];
   };
 }
