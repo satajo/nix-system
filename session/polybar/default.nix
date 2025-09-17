b95c4b271bd452107b05d22f00845c45c7de22a7
Sami Jokela <sami.jokela@satajo.com>
2025-09-17 11:52:40 +0200

    Use a custom systemd unit for polybar
    The home-manager one seems to rely on some internal target that is not active,
    and which cannot be easily made to be activated. Instead, let's just define a
    unit by ourselves and be done with it.

--- session/polybar/default.nix
+++ session/polybar/default.nix
@@ -1,17 +1,30 @@
 { pkgs, ... }:
 let
   theme = import ../../theme/lib.nix { pkgs = pkgs; };
+  configFile = theme.substitute ./config.template.ini;
 in
 {
   home-manager.users.satajo = {
     home.packages = with pkgs; [
+      polybar
       playerctl
     ];
 
-    services.polybar = {
-      enable = true;
-      script = "polybar primary &";
-      config = theme.substitute ./config.template.ini;
+    systemd.user.services.polybar = {
+      Unit = {
+        Description = "Polybar status bar";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.polybar}/bin/polybar --config=${configFile} primary";
+        Restart = "always";
+        RestartSec = "1s";
+      };
+
+      Install.WantedBy = [ "graphical-session.target" ];
     };
   };
 }
