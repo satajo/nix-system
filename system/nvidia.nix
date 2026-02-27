fc87867ba6479cfa0d2069367aee9e51f22f3fd6
Sami Jokela <sami.jokela@satajo.com>
2026-02-27 20:12:52 +0100

    Fix nvidia crashes in fullscreen video playback

--- system/nvidia.nix
+++ system/nvidia.nix
@@ -4,6 +4,9 @@
     # Defining nvidia as xserver video drivers enables a host of nvidia features.
     services.xserver.videoDrivers = [ "nvidia" ];
 
+    # Force full composition pipeline to prevent crashes because of POS drivers.
+    hardware.nvidia.forceFullCompositionPipeline = true;
+
     # Use the closed-source driver.
     hardware.nvidia.open = false;
 
